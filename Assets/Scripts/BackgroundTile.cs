f6f0a76feefbca0d8d04e701774c61e16a2c9486
gbvsCloud <gabriielblock@gmail.com>
2023-09-27 15:09:07 -0300

    Basic map generation

--- Assets/Scripts/BackgroundTile.cs
+++ Assets/Scripts/BackgroundTile.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class BackgroundTile : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public SpriteRenderer sr;
     void Start()
     {
+        int randomColor = Random.Range(0, 3);
+
+        if (randomColor == 0)
+            sr.color = Color.red;
+        else if(randomColor == 1)
+            sr.color = Color.green;
+        else
+            sr.color = Color.blue;
         
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+
     void Start()
     {
-        
+        width = 5;
+        height = 8;
+
+        allTiles = new BackgroundTile[width, height];  
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Instantiate(tilePrefab, new Vector2(transform.position.x + i, transform.position.y + j), Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+           
     }
 }
